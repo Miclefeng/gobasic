93a48592a25f7c5f5aaa1a0332a71e0af2f20446
ZSShang <1280054628@qq.com>
2018-04-11 00:38:35 +0800

    edit

--- container/nonrepeating.go
+++ container/nonrepeating.go
@@ -9,9 +9,11 @@ import (
 // lastOccurred[x] >= start 更新start
 // 更新lastOccurred[x],更新maxLength
 func lengthOfNonRepeatingSubStr(s string) int {
-	lastOccured := make(map[byte]int) // 记录每个字符最后出现的位置
+	//lastOccured := make(map[byte]int) // 记录每个字符最后出现的位置
+	lastOccured := make(map[rune]int) // 支持中文
 	start, maxLength := 0, 0
-	for i, ch := range []byte(s) {
+	//for i, ch := range []byte(s) {
+	for i, ch := range []rune(s) { // 支持中文
 		lastI, ok := lastOccured[ch]
 		if ok && lastI >= start {
 			start = lastOccured[ch] + 1
@@ -38,4 +40,6 @@ func main() {
 		lengthOfNonRepeatingSubStr("b"))
 	fmt.Println(
 		lengthOfNonRepeatingSubStr("abcdefg"))
+	fmt.Println(
+		lengthOfNonRepeatingSubStr("黑化肥会发会发挥"))
 }
--- container/stringsRUNE.go
+++ container/stringsRUNE.go
@@ -1,5 +1,41 @@
 package main
 
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
 func main() {
-	
+	s := "Yes我爱慕课网!" // utf-8
+	fmt.Println(len(s)) // len 获取字节数
+	fmt.Printf("%s\n", []byte(s))
+	fmt.Printf("%x\n", []byte(s))
+	for _, b := range []byte(s) {
+		fmt.Printf("%X ", b)
+	}
+	fmt.Println()
+	for i, ch := range s { // ch is a rune,对utf-8解码,在进行unicode
+		fmt.Printf("(%d %X) ", i, ch)
+	}
+	fmt.Println()
+
+	// 返回字符的长度
+	fmt.Println("Rune count: ", utf8.RuneCountInString(s))
+
+	bytes := []byte(s) // []byte 获取所有的字节
+	ch, size := utf8.DecodeRune(bytes)
+	fmt.Println(size, ch, bytes)
+	fmt.Println()
+	for len(bytes) > 0 {
+		ch, size := utf8.DecodeRune(bytes) // 只返回第一个字符和字符所占长度
+		bytes = bytes[size:] // 分片往后截取字符,每次去除第一个字符
+		//fmt.Println(size, ch, bytes)
+		fmt.Printf("%c ", ch)
+	}
+	fmt.Println()
+
+	for i, ch := range []rune(s) { // rune 每个字符占4个字节
+		fmt.Printf("(%d %c) ", i, ch)
+	}
+	fmt.Println()
 }
