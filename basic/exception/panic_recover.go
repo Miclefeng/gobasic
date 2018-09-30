08e1426ce4308f0dd907cf509ba416783193e24e
miclefeng <miclefengzss@163.com>
2018-09-30 11:50:01 +0800

    edit

--- basic/exception/panic_recover.go
+++ basic/exception/panic_recover.go
@@ -9,6 +9,7 @@ func testa() {
 func testb(x int)  {
 	// defer 函数在函数结束后执行，多个defer(使用栈进行存储)遵循先进后出原则
 	defer func() {
+		// recover() 可以打印panic中的错误信息，恢复panic退出的程序
 		if err := recover(); err != nil {
 			fmt.Println(err)
 		}
