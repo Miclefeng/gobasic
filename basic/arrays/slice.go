fcdde6dcfeaac1397ae76ad1e8cceeafc5e396a9
miclefeng <miclefengzss@gmail.com>
2018-10-31 10:57:26 +0800

    edit

--- basic/arrays/slice.go
+++ basic/arrays/slice.go
@@ -5,6 +5,32 @@ import (
 	"strings"
 )
 
+func removeElement(nums []int, val int) int {
+	length := len(nums)
+	j := 0
+	newNums := []int{}
+	for i := 0; i < length; i++ {
+		if nums[j] == val {
+			newNums = append(newNums, nums[j])
+			nums = append(nums[0:j], nums[j+1:]...)
+		} else {
+			j++
+		}
+	}
+	return len(newNums)
+}
+
+func removeElement2(nums []int, val int) int {
+	for i := 0; i < len(nums); {
+		if nums[i] == val {
+			nums = append(nums[0:i], nums[i+1:]...)
+		} else {
+			i++
+		}
+	}
+	return len(nums)
+}
+
 func main() {
 	a := make([]int, 0, 1)
 	ocap := cap(a)
@@ -20,4 +46,10 @@ func main() {
 	str := "test"
 	sl := strings.Split(str, ",")
 	fmt.Println(sl[0])
+	fmt.Println()
+	s2 := []int{0,1,2,2,3,0,4,2}
+	s2 = []int{3, 2, 2, 3}
+	val := 2
+	length := removeElement(s2, val)
+	fmt.Println(length)
 }
