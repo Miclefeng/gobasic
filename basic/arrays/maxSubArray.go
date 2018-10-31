e6d1d9f4a7c9f47b317e1330f697cb3653146a59
miclefeng <miclefengzss@gmail.com>
2018-10-31 18:11:21 +0800

    edit

---
+++ basic/arrays/maxSubArray.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+)
+
+func maxSubArray(nums []int) int {
+	// 分配二维数组保存每次计算结果
+	res := make([][]int, len(nums))
+	maxNum := nums[0]
+	for i := 0; i < len(nums); i++ {
+		// 保存开始的元素
+		res[i] = append(res[i], nums[i])
+		for j := i + 1; j < len(nums); j++ {
+			// 保存本次计算结果
+			res[i] = append(res[i], res[i][j-i-1]+nums[j])
+			if maxNum < res[i][j-i-1]+nums[j] {
+				maxNum = res[i][j-i-1] + nums[j]
+			}
+			fmt.Println(res[i][j-i-1] + nums[j])
+		}
+		fmt.Println(res[i])
+		if maxNum < res[i][0] {
+			maxNum = res[i][0]
+		}
+	}
+	return maxNum
+}
+
+/*
+	这是一个典型的动态规划问题
+	用i 做哨兵，根据题目理解，需要找出一个连续的子数组，使得和最大，这时候，哨兵i 要么是前面子数组的一份子，要么是新数组的起始
+	而maxsum 代表了，在循序遍历数组时，左右子数组的和的最大值
+	因此 前面子数组的和 我们用currentsum 表示，就出现了下面的公式
+	currentSum = max(a[i], currentSum + a[i])
+	maxSum = max(currentSum, maxSum)
+
+	这是典型的动态规划的问题，只要深刻的理解了动态规划，你就发现这就是几行代码搞定的问题
+*/
+func maxSubArray2(nums []int) int {
+	maxsum := nums[0]
+	currentSum := 0
+	for i := 0; i < len(nums); i++ {
+		currentSum = getMax(nums[i]+currentSum, nums[i])
+		maxsum = getMax(maxsum, currentSum)
+		fmt.Println(currentSum, maxsum)
+	}
+	return int(maxsum)
+}
+func getMax(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	nums := []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}
+	//nums = []int{1,4}
+	//maxNum := maxSubArray(nums)
+	maxNum := maxSubArray2(nums)
+	fmt.Println(maxNum)
+}
--- basic/func/fib.go
+++ basic/func/fib.go
@@ -5,10 +5,13 @@ import (
 	"time"
 )
 
-const LIM  = 41
+const LIM  = 10
+
 
 var fibs[LIM] int64
 
+// 自顶向下求解法
+// 运用 slice存储上一次的结果，又叫 带备忘的自顶向下算法
 func fibonacci(n int) (res int64) {
 	if fibs[n] != 0 {
 		res = fibs[n]
@@ -24,6 +27,24 @@ func fibonacci(n int) (res int64) {
 	return 
 }
 
+// 自底向上的算法
+// f(0) = f(1) = 1
+// f(2) = f(1) + f(0)
+// f(3) = f(2) + f(1)
+// f(4) = f(3) + f(2)
+func fibonacci2(n int) int {
+	if n <= 1 {
+		return 1
+	}
+
+	a := 1
+	b := 1
+	for i := 2; i <= n; i++ {
+		a, b = b, a + b
+	}
+	return b
+}
+
 func main()  {
 	var result int64
 	start := time.Now()
@@ -36,4 +57,9 @@ func main()  {
 	fmt.Println(start.UnixNano())
 	fmt.Println(end.UnixNano())
 	fmt.Printf("Calculation is %d\n", delta)
+	fmt.Println()
+	for i := 0; i < 10; i ++ {
+		res := fibonacci2(i)
+		fmt.Printf("fibonacci(%d) is %d\n", i, res)
+	}
 }
