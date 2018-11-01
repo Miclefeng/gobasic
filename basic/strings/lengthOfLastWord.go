433264114efe38b359fb5447be6a4ee480a9d68e
miclefeng <miclefengzss@gmail.com>
2018-11-01 10:05:33 +0800

    edit

--- basic/strings/lengthOfLastWord.go
+++ basic/strings/lengthOfLastWord.go
@@ -4,14 +4,18 @@ import "fmt"
 
 func lengthOfLastWord(s string) int {
 	start := 0
+	// ' ' 标记
 	ok := true
 	for _, v := range s {
 		if ' ' == v {
+			// 标记为 true
 			ok = true
 		} else {
+			// 如果' '之后有字符，长度赋值为0
 			if ok {
 				start = 0
 			}
+			// 标记为 false
 			ok = false
 			start++
 		}
