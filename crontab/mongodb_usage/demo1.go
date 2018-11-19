90b1fcf80621a100110770e276b488679e18aaf1
miclefeng <miclefengzss@gmail.com>
2018-11-19 13:57:39 +0800

    edit

---
+++ crontab/mongodb_usage/demo1.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"github.com/mongodb/mongo-go-driver/mongo"
+	"github.com/mongodb/mongo-go-driver/options"
+	"golang.org/x/net/context"
+	"time"
+)
+
+/**
+ * Author : MicleFengzss@gmail.com
+ * Time : 2018/11/19 上午11:23
+ */
+
+func main() {
+	var (
+		err error
+		client *mongo.Client
+		clientOption *options.ClientOptions
+		db *mongo.Database
+		collection *mongo.Collection
+	)
+
+	clientOption = options.Client().SetConnectTimeout(1*time.Second)
+	// 创建客户端连接
+	if client, err = mongo.Connect(context.TODO(), "mongodb://127.0.0.1:27017", clientOption); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	db = client.Database("my_db")
+
+	collection = db.Collection("my_collection")
+
+	collection = collection
+}
