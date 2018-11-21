7e89e171fd32c8814d379acf26f2d46e31fd3290
miclefeng <miclefengzss@gmail.com>
2018-11-21 10:20:11 +0800

    edit

--- crawler/main.go
+++ crawler/main.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
+	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
 )
 
@@ -18,6 +25,7 @@ func main() {
 		err error
 		resp *http.Response
 		result []byte
+		encodingReader *transform.Reader
 	)
 
 	if resp, err = http.Get(Url); err != nil {
@@ -30,10 +38,29 @@ func main() {
 		fmt.Println("Error status code: ", resp.StatusCode)
 		return
 	}
-
-	if result, err = ioutil.ReadAll(resp.Body); err != nil {
+	// 转换页面内容的编码格式
+	encodingReader = transform.NewReader(resp.Body, determineEncoding(resp.Body).NewEncoder())
+	// 读取页面内容
+	if result, err = ioutil.ReadAll(encodingReader); err != nil {
 		fmt.Println(err)
 		return
 	}
 	fmt.Printf("%s\n", result)
+}
+
+ // 判断页面内容的编码格式
+func determineEncoding(r io.Reader) (e encoding.Encoding) {
+	var (
+		bytes []byte
+		err error
+	)
+
+	if bytes, err = bufio.NewReader(r).Peek(1024); err != nil {
+		log.Fatalf("bufio peek err: %v\n", err);
+		e = unicode.UTF8
+		return e
+	}
+
+	e, _, _ = charset.DetermineEncoding(bytes, "")
+	return e
 }
