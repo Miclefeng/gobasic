1b9ec7b28557757a196f0a6dacba605048aaed78
miclefeng <miclefengzss@gmail.com>
2018-11-26 15:32:26 +0800

    edit

--- src/crontab/miclefeng/worker/Executor.go
+++ src/crontab/miclefeng/worker/Executor.go
@@ -35,6 +35,9 @@ func (executor *Executor) ExecuteJob(jobExecuteInfo *common.JobExecuteInfo) {
 			OutPut:         make([]byte, 0),
 		}
 
+		// 任务开始执行时间
+		executeResult.StartTime = time.Now()
+
 		// 初始化分布式锁
 		jobLock = G_jobManager.CreateJobLock(executeResult.JobExecuteInfo.Job.Name)
 		// 尝试上锁
@@ -42,18 +45,24 @@ func (executor *Executor) ExecuteJob(jobExecuteInfo *common.JobExecuteInfo) {
 		// 释放锁
 		defer jobLock.UnLock()
 
-		// 任务开始执行时间
-		executeResult.StartTime = time.Now()
+		if err != nil {
+			executeResult.Error = err
+			executeResult.EndTime = time.Now()
+		} else {
+			// 上锁成功后，重置任务开始时间
+			executeResult.StartTime = time.Now()
 
-		// 执行命令
-		cmd = exec.CommandContext(context.TODO(), "/bin/bash", "-c", jobExecuteInfo.Job.Command)
-		// 捕获输出
-		outPut, err = cmd.CombinedOutput()
-		executeResult.OutPut = outPut
-		executeResult.Error = err
-		// 任务结束时间
-		executeResult.EndTime = time.Now()
-		fmt.Println("执行结果：", jobExecuteInfo.Job.Name, string(outPut), err, executeResult.EndTime)
+			// 执行命令
+			cmd = exec.CommandContext(context.TODO(), "/bin/bash", "-c", jobExecuteInfo.Job.Command)
+			// 捕获输出
+			outPut, err = cmd.CombinedOutput()
+			executeResult.OutPut = outPut
+			executeResult.Error = err
+			// 任务结束时间
+			executeResult.EndTime = time.Now()
+			fmt.Println("执行结果：", jobExecuteInfo.Job.Name, string(outPut), err, executeResult.EndTime)
+		}
+
 		// 任务执行完成后，把执行的结果返回给Scheduler，Scheduler会从executingTable(内存表)中删除掉执行记录
 		G_scheduler.PushJobExecuteResult(executeResult)
 	}()
