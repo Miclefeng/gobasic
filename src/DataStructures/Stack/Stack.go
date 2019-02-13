1a142ef7599a009420df1e80921270b00ca17d6c
miclefeng <miclefengzss@gmail.com>
2019-02-13 14:52:26 +0800

    edit

---
+++ src/DataStructures/Stack/Stack.go
@@ -0,0 +1,62 @@
+package Stack
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+/**
+ * Author : MicleFengzss@gmail.com
+ * Time : 2019/2/13 下午2:29
+ */
+
+type Stack []interface{}
+
+// 入栈
+func (stack *Stack) Push(e interface{}) {
+	*stack = append(*stack, e)
+}
+
+// 出栈
+func (stack *Stack) Pop() (e interface{}) {
+	if 0 == stack.Len() {
+		return nil
+	}
+	e = (*stack)[stack.Len()-1]
+	*stack = (*stack)[0 : stack.Len()-1]
+	return
+}
+
+// 查看栈顶元素
+func (stack *Stack) Top() (e interface{}) {
+	if 0 == stack.Len() {
+		return nil
+	}
+
+	e = (*stack)[stack.Len()-1]
+	return
+}
+
+// 栈的元素个数
+func (stack *Stack) Len() int {
+	return len(*stack)
+}
+
+func (stack *Stack) Print() {
+	fmt.Printf("Stack: length = %d\n", stack.Len())
+	str := "["
+	for i := 0; i < stack.Len(); i++ {
+		switch (*stack)[i].(type) {
+		case int:
+			str += strconv.Itoa((*stack)[i].(int)) + ", "
+		case float64:
+			str += strconv.FormatFloat((*stack)[i].(float64), 'E', -1, 64) + ", "
+		default:
+			str += (*stack)[i].(string) + ", "
+		}
+	}
+	str = strings.TrimRight(str, ", ")
+	str += "] -> Top"
+	fmt.Println(str)
+}
---
+++ src/DataStructures/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"DataStructures/Stack"
+	"fmt"
+)
+
+/**
+ * Author : MicleFengzss@gmail.com
+ * Time : 2019/2/13 下午2:43
+ */
+
+func main() {
+	stack := &Stack.Stack{}
+
+	for i := 0;i < 10; i++ {
+		stack.Push(i)
+	}
+	stack.Print()
+	stack.Pop()
+	stack.Print()
+	stack.Pop()
+	stack.Pop()
+	stack.Pop()
+	stack.Print()
+	fmt.Println(stack.Top())
+}
