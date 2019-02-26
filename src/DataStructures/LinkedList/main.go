7487968e53440a45e6cd639d657b550862b6f43f
miclefeng <miclefengzss@gmail.com>
2019-02-26 10:16:08 +0800

    edit

---
+++ src/DataStructures/LinkedList/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"DataStructures/LinkedList/LinkedListStack"
+)
+
+/**
+ * Author : MicleFengzss@gmail.com
+ * Time : 2019/2/21 上午10:20
+ */
+
+func main() {
+	//linkedList := LinkedList.Instance
+	//
+	//for i := 0; i < 10; i++ {
+	//	linkedList.AddLast(i)
+	//}
+	//
+	//fmt.Println(linkedList.GetSize())
+	//fmt.Println()
+	//for i := 0; i < linkedList.GetSize(); i++ {
+	//	fmt.Println(linkedList.Get(i))
+	//}
+	//
+	//linkedList.Set(5, "a")
+	//fmt.Println(linkedList.Get(5))
+	//fmt.Println(linkedList.Contains("a"))
+	//fmt.Println(linkedList.Contains("b"))
+	//fmt.Println("---------------------")
+	//fmt.Println(linkedList.RemoveFirst())
+	//fmt.Println(linkedList.RemoveLast())
+	//linkedList.RemoveElement("a")
+	//fmt.Println("---------------------")
+	//for i := 0; i < linkedList.GetSize(); i++ {
+	//	fmt.Println(linkedList.Get(i))
+	//}
+	//
+	//fmt.Println("--------------------")
+
+	listStack := LinkedListStack.Instance
+	for i := 0; i < 10; i++ {
+		listStack.Push(i)
+		listStack.Print()
+		if i%3 == 2 {
+			listStack.Pop()
+			listStack.Print()
+		}
+	}
+}
