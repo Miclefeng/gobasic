16de977e6a0e7529e12a12a24cea2f8f82fde7de
miclefeng <miclefengzss@gmail.com>
2019-03-07 09:44:30 +0800

    edit

--- src/DataStructures/Tree/BinarySearchTree/BST.go
+++ src/DataStructures/Tree/BinarySearchTree/BST.go
@@ -23,7 +23,9 @@ func init() {
 }
 
 func (bst *BST) Add(key int, e interface{}) {
+	// 先生成一个树节点
 	newNode := &Node.Node{Key: key, E: e}
+
 	if nil == bst.root {
 		bst.root = newNode
 		bst.size++
@@ -57,4 +59,12 @@ func (bst *BST) add(node, newNode *Node.Node) {
 			bst.add(node.Right, newNode)
 		}
 	}
+}
+
+func (bst *BST) GetSize() int {
+	return bst.size
+}
+
+func (bst *BST) IsEmpty() bool {
+	return 0 == bst.size
 }
