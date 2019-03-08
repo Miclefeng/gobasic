d9cb9b6c475481cf80545872c68d94d804ebfb1e
miclefeng <miclefengzss@gmail.com>
2019-03-08 15:31:22 +0800

    edit

--- src/DataStructures/Tree/BinarySearchTree/BST.go
+++ src/DataStructures/Tree/BinarySearchTree/BST.go
@@ -2,6 +2,7 @@ package BinarySearchTree
 
 import (
 	"DataStructures/Tree/Node"
+	"fmt"
 )
 
 /**
@@ -19,9 +20,31 @@ var (
 )
 
 func init() {
-	Instance = &BST{Node.Instance, 0}
+	Instance = new(BST)
 }
 
+// 向二分搜索树添加节点
+func (bst *BST) NewAdd(key int, e interface{}) {
+	bst.root = bst.newAdd(key, e, bst.root)
+}
+
+// 递归向二分搜索树添加节点
+func (bst *BST) newAdd(key int, e interface{}, node *Node.Node) *Node.Node {
+	if nil == node {
+		bst.size++
+		return &Node.Node{Key: key, E: e}
+	}
+
+	if key < node.Key {
+		node.Left = bst.newAdd(key, e, node.Left)
+	}
+	if key > node.Key {
+		node.Right = bst.newAdd(key, e, node.Right)
+	}
+	return node
+}
+
+// 向二分搜索树添加节点
 func (bst *BST) Add(key int, e interface{}) {
 	// 先生成一个树节点
 	newNode := &Node.Node{Key: key, E: e}
@@ -34,8 +57,9 @@ func (bst *BST) Add(key int, e interface{}) {
 	}
 }
 
+// 递归向二分搜索树添加节点
 func (bst *BST) add(node, newNode *Node.Node) {
-	if node.E == newNode.E {
+	if node.Key == newNode.Key {
 		return
 	}
 
@@ -61,10 +85,73 @@ func (bst *BST) add(node, newNode *Node.Node) {
 	}
 }
 
+// 在二分搜索树中查找某个元素
+func (bst *BST) Search(key int) bool {
+	return bst.search(bst.root, key)
+}
+
+// 递归遍历在二分搜索树中查找某个元素
+func (bst *BST) search(node *Node.Node, key int) bool {
+	if nil == node {
+		return false
+	}
+	if key < node.Key {
+		return bst.search(node.Left, key)
+	}
+	if key > node.Key {
+		return bst.search(node.Right, key)
+	}
+	return true
+}
+
+// 前序遍历
+func (bst *BST) PreOrder() {
+	preOrder(bst.root)
+}
+
+func preOrder(node *Node.Node) {
+	if nil == node {
+		return
+	}
+	fmt.Printf("%s\t", node.E)
+	preOrder(node.Left)
+	preOrder(node.Right)
+}
+
+// 中序遍历
+func (bst *BST) InOrder() {
+	inOrder(bst.root)
+}
+
+func inOrder(node *Node.Node) {
+	if nil == node {
+		return
+	}
+	inOrder(node.Left)
+	fmt.Printf("%s\t", node.E)
+	inOrder(node.Right)
+}
+
+// 后序遍历
+func (bst *BST) PostOrder() {
+	postOrder(bst.root)
+}
+
+func postOrder(node *Node.Node) {
+	if nil == node {
+		return
+	}
+	postOrder(node.Left)
+	postOrder(node.Right)
+	fmt.Printf("%s\t", node.E)
+}
+
+// 获取树节点个数
 func (bst *BST) GetSize() int {
 	return bst.size
 }
 
+// 判断是否是空
 func (bst *BST) IsEmpty() bool {
 	return 0 == bst.size
 }
---
+++ src/DataStructures/Tree/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"DataStructures/Tree/BinarySearchTree"
+	"fmt"
+	"strconv"
+)
+
+/**
+ * Author : MicleFengzss@gmail.com
+ * Time : 2019/3/8 下午2:44
+ */
+
+func main() {
+	bst := BinarySearchTree.Instance
+
+	arr := []int{5, 3, 8, 2, 6, 4, 9}
+
+	for _, v := range arr {
+		bst.NewAdd(v, strconv.Itoa(v))
+	}
+	fmt.Println(bst.GetSize())
+	fmt.Println("-------------------------")
+	bst.PreOrder()
+	fmt.Println()
+	fmt.Println("-------------------------")
+	bst.InOrder()
+	fmt.Println()
+	fmt.Println("-------------------------")
+	bst.PostOrder()
+}
