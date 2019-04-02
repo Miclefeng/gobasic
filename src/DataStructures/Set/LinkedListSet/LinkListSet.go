f871f047f9fa8a2db792988d99486d3ddd6cdc76
miclefeng <miclefengzss@gmail.com>
2019-04-02 09:36:34 +0800

    linkedlist set

---
+++ src/DataStructures/Set/LinkedListSet/LinkListSet.go
@@ -0,0 +1,47 @@
+package LinkedListSet
+
+import (
+	"DataStructures/LinkedList/LinkedList"
+	_ "DataStructures/Set/Set"
+)
+
+/**
+ * Author : MicleFengzss@gmail.com
+ * Time : 2019/4/2 上午9:20
+ */
+
+type LinkedListSet []interface{}
+
+var (
+	lList    *LinkedList.LinkedList
+	Instance *LinkedListSet
+)
+
+func init() {
+	lList = LinkedList.Instance
+	Instance = new(LinkedListSet)
+}
+
+func (lSet *LinkedListSet) GetSize() int {
+	return lList.GetSize()
+}
+
+func (lSet *LinkedListSet) IsEmpty() bool {
+	return 0 == lList.GetSize()
+}
+
+func (lSet *LinkedListSet) Add(e interface{}) {
+	if !lList.Contains(e) {
+		lList.AddFirst(e)
+	}
+}
+
+func (lSet *LinkedListSet) Remove(e interface{}) {
+	lList.RemoveElement(e)
+}
+
+func (lSet *LinkedListSet) Contains(e interface{}) bool {
+	return lList.Contains(e)
+}
+
+
--- src/DataStructures/Set/main.go
+++ src/DataStructures/Set/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"DataStructures/Set/BinarySearchTreeTSet"
 	"DataStructures/Set/FileOperation"
+	"DataStructures/Set/LinkedListSet"
 	"fmt"
 )
 
@@ -29,4 +30,24 @@ func main()  {
 		bstSet2.Add(v)
 	}
 	fmt.Println("Total different words: ", bstSet2.GetSize())
+
+	fmt.Println("----------------------------------")
+
+	fmt.Println("Pride and Prejudice")
+	words3 := FileOperation.ReadFile("src/DataStructures/Set/pride-and-prejudice.txt")
+	fmt.Println("Total words: ", len(words3))
+	lSet := LinkedListSet.Instance
+	for _, v  := range words3 {
+		lSet.Add(v)
+	}
+	fmt.Println("Total different words: ", lSet.GetSize())
+
+	fmt.Println("A Tale of Two Cities")
+	words4 := FileOperation.ReadFile("src/DataStructures/Set/a-tale-of-two-cities.txt")
+	fmt.Println("Total words: ", len(words4))
+	lSet2 := LinkedListSet.Instance
+	for _, v  := range words4 {
+		lSet2.Add(v)
+	}
+	fmt.Println("Total different words: ", lSet2.GetSize())
 }
