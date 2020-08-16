4ad3dcf8cfb6aad30875f5c9ee3e5e37eee765db
miclefengzss <miclefengzss@163.com>
2020-08-16 08:35:50 +0800

    edit

--- src/code/Algorithm/merge.go
+++ src/code/Algorithm/merge.go
@@ -2,24 +2,32 @@ package main
 
 import "fmt"
 
-func mergeSort(arr []int, l , r int)  {
+func MergeSort(arr []int) {
+	length := len(arr)
+	if length == 0 || length == 1 {
+		return
+	}
+	mergeSort(arr, 0, length - 1)
+}
+
+func mergeSort(arr []int, l, r int) {
 	if l >= r {
 		return
 	}
 	m := (l + r) / 2
 	mergeSort(arr, l, m)
-	mergeSort(arr, m + 1, r)
-	if arr[m] > arr[m + 1] {
+	mergeSort(arr, m+1, r)
+	if arr[m] > arr[m+1] {
 		merge(arr, l, m, r)
 	}
 }
 
-func merge(arr []int, l, m, r int)  {
-	aux := make([]int, r - l + 1, r + 1)
+func merge(arr []int, l, m, r int) {
+	aux := make([]int, r-l+1, r+1)
 	//aux := []int{}
 
 	for i := l; i <= r; i++ {
-		aux[i - l] = arr[i]
+		aux[i-l] = arr[i]
 		// append 会自动扩容切片
 		//aux = append(aux, arr[i])
 	}
@@ -28,16 +36,16 @@ func merge(arr []int, l, m, r int)  {
 	j := m + 1
 	for k := l; k <= r; k++ {
 		if i > m {
-			arr[k] = aux[j - l]
+			arr[k] = aux[j-l]
 			j++
 		} else if j > r {
-			arr[k] = aux[i - l]
+			arr[k] = aux[i-l]
 			i++
-		} else if aux[j - l] > aux[i - l] {
-			arr[k] = aux[i - l]
+		} else if aux[j-l] > aux[i-l] {
+			arr[k] = aux[i-l]
 			i++
 		} else {
-			arr[k] = aux[j - l]
+			arr[k] = aux[j-l]
 			j++
 		}
 	}
@@ -45,8 +53,6 @@ func merge(arr []int, l, m, r int)  {
 
 func main() {
 	arr := []int{11, 13, 6, 9, 4, 8, 16}
-	start := 0
-	end := len(arr) - 1
-	mergeSort(arr, start, end)
+	MergeSort(arr)
 	fmt.Println(arr)
 }
